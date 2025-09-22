6376c7e1bbf32ca3a1ed0d241aeaf07d51462bd2
Denis <koprmalak212@gmail.com>
2025-09-22 14:24:34 +0200

    Added Duck clicker with no upgrades

--- scripts/DUCKclick.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(e) {
-
-    const click = document.getElementById("click");
-    click.addEventListener("click", function(){
-        alert("clicked!");
-    })
-
-
-})
